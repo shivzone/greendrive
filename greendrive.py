a043b9f707eecc0ec406a5af518bd1c00073ad18
Shivram Mani <shivram.mani@gmail.com>
2019-10-17 13:57:07 -0700

    working in use and availability count

--- greendrive.py
+++ greendrive.py
@@ -3,11 +3,16 @@
 """
 import os
 import sys
+import time
 from slackNotification import SlackNotification
 
 from zeep import Client
 from zeep.wsse.username import UsernameToken
 from datetime import datetime, timedelta
+from pytz import timezone
+
+availableCount = 0
+inUseCount = 0
 
 def makeUsageAPIcall(client, tStart, tEnd):
 	"""
@@ -15,62 +20,85 @@ def makeUsageAPIcall(client, tStart, tEnd):
 						E.g. tStart=datetime(2018, 5, 30, 00, 00, 00)
 	Note: the tEnd parameter will be set to the end of the day, e.g. datetime(2018, 5, 30, 23, 59, 59)
 	"""
-	print("Making usage API query..")
 	#tEnd = tStart + timedelta(hours=23, minutes=59, seconds=59)
 	usageSearchQuery = {
 		'fromTimeStamp': tStart,
-		'toTimeStamp': tEnd,
-	}
+		'toTimeStamp': tEnd	}
 	data = client.service.getChargingSessionData(usageSearchQuery)
 	# print("Number of records in time-frame: ", len(data.ChargingSessionData))
 
 	## Fill Sessions Table
 	for d in data.ChargingSessionData:
-		## enclose in try-except to avoid TypeError: int() argument must be a string or a number, not 'NoneType'
-		## when userID is None, and other errors
 		try:
-			row_session = [int(d.sessionID), d.startTime.strftime('%Y-%m-%d %H:%M:%S'), 
-							d.endTime.strftime('%Y-%m-%d %H:%M:%S'), float(d.Energy), 
+			row_session = [int(d.sessionID), d.startTime.strftime('%Y-%m-%d %H:%M:%S'),
+							d.endTime.strftime('%Y-%m-%d %H:%M:%S'), float(d.Energy),
 							str(d.stationID), int(d.portNumber), int(d.userID)]
 			print(row_session)
 		except:
 			pass
 
 
-def makeStationAPIcall(client):
-	print("Making station API query..")
+def makeStationStatuscall(client):
 	## Get station data
 	searchQuery = {}
-	stationData = client.service.getStations(searchQuery)
+
+	stationData = client.service.getStationStatus(searchQuery)
 	numStations = len(stationData.stationData)
-	print(numStations)
+	i = 0
+	inuse = 0
+	available = 0
+	while (i < numStations):
+		numPorts = len(stationData.stationData[i].Port)
+		j = 0
+		while j < numPorts:
+			if stationData.stationData[i].Port[j].Status == 'AVAILABLE':
+				available = available + 1
+			elif stationData.stationData[i].Port[j].Status == 'INUSE':
+				inuse = inuse + 1
+			j = j + 1
+		i = i + 1
 
-	print(stationData)
+	print("Stations: InUse = {}, Available = {}".format(inuse, available))
+	return inuse, available
 
-	## Fill Pricing Table Rows - assumes only one pricing model exists currently
-	#price = stationData.stationData[0].Pricing[0]
-        #print price
+#getPublicStationStatus shows status of usage
 
 def main():
-	startTime = datetime(2019, 10, 16, 9, 00, 00) - timedelta(hours=7, minutes=0, seconds=0)
-	endTime = datetime(2019, 10, 17, 18, 00, 00) - timedelta(hours=7, minutes=0, seconds=0)
+	utc = timezone('UTC')
+	startTime = datetime(2019, 10, 17, 9, 00, 00).astimezone(utc)#- timedelta(hours=7, minutes=0, seconds=0)
+	endTime = datetime(2019, 10, 19, 18, 00, 00).astimezone(utc)#- timedelta(hours=7, minutes=0, seconds=0)
         
 	if len(sys.argv) < 3:
 		print ("greendrive needs username(key) and password")
 		sys.exit(1)
 	username = sys.argv[1]
 	password = sys.argv[2]
+	slack_token = sys.argv[3]
 
-	wsdl_url = "https://webservices.chargepoint.com/cp_api_5.0.wsdl"
+	wsdl_url = "https://webservices.chargepoint.com/cp_api_5.1.wsdl"
 	client = Client(wsdl_url, wsse=UsernameToken(username, password))
+	slack = SlackNotification(slack_token, "#greendrive-hackday-2019")
+	global inUseCount, availableCount
 
-	#makeStationAPIcall(client) ## can make some check: if len(getStations)
+	while True:
+		inuse, available = makeStationStatuscall(client)
+		if inuse != inUseCount or available != availableCount:
+			inUseCount = inuse
+			availableCount = available
+			if availableCount == 0:
+				slack.sendMessage("There are no available stations at this time")
+			else:
+				slack.sendMessage("There are now {} available charging stations".format(availableCount))
+		time.sleep(60)
+	# if(return) {
+	# 	notify on slack with whats available
+	# }
 
-	makeUsageAPIcall(client, startTime, endTime)
+	# makeUsageAPIcall(client, startTime, endTime)
 	print("Completed ...")
 
 	slack = SlackNotification(sys.argv[3], "#greendrive-hackday-2019")
-	slack.sendMessage("It works!")
+	#slack.sendMessage("It works!")
 
 if __name__== '__main__':
 	main()
